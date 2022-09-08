d62713c18781bd047f553a0ac4d596b370cbf10f
jkrosby51 <56936867+jkrosby51@users.noreply.github.com>
2022-09-08 13:13:36 -0400

    Update launchPad.py

--- raspberry-pi/launchPad.py
+++ raspberry-pi/launchPad.py
@@ -3,6 +3,8 @@ import board
 import digitalio
 import time
 import math
+import pwmio
+from adafruit_motor import servo
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
@@ -13,6 +15,9 @@ gled = digitalio.DigitalInOut(board.GP15)
 gled.direction = digitalio.Direction.OUTPUT
 btn = digitalio.DigitalInOut(board.GP13)
 btn.switch_to_input(pull=digitalio.Pull.DOWN)
+
+pwm_servo = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 15, frequency=50)
+servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2200)   # pulse may need to be tuned to specific servo
 
 launched = True
 
@@ -25,33 +30,31 @@ while True:
         gled.value = False
         print("Launch Countdown Starting")
         x = 10
-        while True:
+        while x > 0:
             cdCurrent = time.time() - cdStart
             print(math.floor(cdCurrent*10)/10)
-
+            
             if (math.floor(cdCurrent*10)/10) not in printedtimes:
                 if (math.floor(cdCurrent*10)/10) == 0.0:
-                    #print(x)
-                    #x = x - 1
                     print("rled ON")
-                    #rled.value = True     # blinks on for half a second and then off for half a second, every second during the cooldown
+                    rled.value = True        # blinks on for half a second and then off for half a second, every second during the cooldown
                 if (math.floor(cdCurrent*10)/10) == 0.5:
-                    print("rled OFF")#rled.value = False
+                    print("rled OFF")        #rled.value = False
                 if (math.floor(cdCurrent*10)/10) > 1.0:
                     print("HI")
                     cdStart = time.time()
                     printedtimes = {}
                 print((math.floor(cdCurrent*10)/10))
                 printedtimes[(math.floor(cdCurrent*10)/10)] = True
-            if btn.value:         # if you press the button during the for loop it will break the for loop, canceling the countdown
+            if btn.value:                     # if you press the button during the for loop it will break the for loop, canceling the countdown
                 launched = False
                 print("ABORT")
                 time.sleep(0.2)
-                break
+                
             
         if launched:
             gled.value = True
             print("Liftoff!");
-        #for x in range(18):    # 18 ticks of 0.05s and 5 degrees moved. Total of 90 degrees in 0.9 seconds.
-        #  servo.angle = servo.angle + 5     !!! psuedo code, will be correctly implemented in the future !!!
-        #  time.sleep(0.05)
+            #for x in range(0.):              # 180 - 0 degrees, -1º at a time.
+            #  servo1.angle = x
+            #  time.sleep(0.05)
