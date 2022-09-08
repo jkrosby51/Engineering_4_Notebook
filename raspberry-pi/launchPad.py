481ebf2dfeb1a0934dfd8e18d1472e38291df88d
jkrosby51 <56936867+jkrosby51@users.noreply.github.com>
2022-09-08 14:10:10 -0400

    Update launchPad.py

--- raspberry-pi/launchPad.py
+++ raspberry-pi/launchPad.py
@@ -16,7 +16,7 @@ gled.direction = digitalio.Direction.OUTPUT
 btn = digitalio.DigitalInOut(board.GP13)
 btn.switch_to_input(pull=digitalio.Pull.DOWN)
 
-pwm_servo = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 15, frequency=50)
+pwm_servo = pwmio.PWMOut(board.GP12, duty_cycle=2 ** 15, frequency=50)
 servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2200)   # pulse may need to be tuned to specific servo
 
 launched = True
@@ -29,10 +29,11 @@ while True:
         launched = True
         gled.value = False
         print("Launch Countdown Starting")
-        x = 10
+        print("10")
+        x = 9
         while x > 0:
             cdCurrent = time.time() - cdStart
-            print(math.floor(cdCurrent*10)/10)
+            #print(math.floor(cdCurrent*10)/10)
             
             if (math.floor(cdCurrent*10)/10) not in printedtimes:
                 if (math.floor(cdCurrent*10)/10) == 0.0:
@@ -41,20 +42,21 @@ while True:
                 if (math.floor(cdCurrent*10)/10) == 0.5:
                     print("rled OFF")        #rled.value = False
                 if (math.floor(cdCurrent*10)/10) > 1.0:
-                    print("HI")
+                    print(x)
+                    x = x - 1
                     cdStart = time.time()
                     printedtimes = {}
                 print((math.floor(cdCurrent*10)/10))
                 printedtimes[(math.floor(cdCurrent*10)/10)] = True
             if btn.value:                     # if you press the button during the for loop it will break the for loop, canceling the countdown
                 launched = False
+                x = -1
                 print("ABORT")
                 time.sleep(0.2)
+        if launched and x == 0:
+                gled.value = True
+                print("Liftoff!");
+                for angle in range(0, 180, 1):    # 180 - 0 degrees, -1º at a time.
+                  servo1.angle = angle
                 
             
-        if launched:
-            gled.value = True
-            print("Liftoff!");
-            #for x in range(0.):              # 180 - 0 degrees, -1º at a time.
-            #  servo1.angle = x
-            #  time.sleep(0.05)
